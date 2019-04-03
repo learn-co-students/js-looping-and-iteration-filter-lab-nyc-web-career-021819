b0475c755f47897b47d7f822ca307c17a4e94986
richietam <richardtam.ny@gmail.com>
2019-04-03 16:50:47 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+const findMatching = function (array, string) {
+  return array.filter(function(driver) {
+    return driver.toLowerCase() === string.toLowerCase()
+  })
+}
+
+const fuzzyMatch = function (drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase().startsWith(string.toLowerCase())
+  })
+}
+
+const matchName = function (drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver["name"] === string
+  })
+}
