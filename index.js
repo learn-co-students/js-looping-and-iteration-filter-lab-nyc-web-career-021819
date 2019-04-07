2e614639be814ebdf2cbae902bb2c76c6b506e7f
Jessy Hu <jesshuy12@gmail.com>
2019-04-07 18:46:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === string.toLowerCase()
+  })//end of filter
+}//end of findMatching
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.slice(0, string.length) === string
+  })//end of filter
+}//end of fuzzyMatch
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.name === string
+  })//end of filter
+}//end of matchName
